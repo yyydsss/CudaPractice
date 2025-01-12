47d745f7f5668211240a471eb0961aa465849194
yyydsss <1978439837@qq.com>
2025-01-12 23:20:27 +0800

    1,2

---
+++ build/4_gemm/CMakeFiles/gemm_1_shared.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gemm_1_shared.dir/gemm_1_shared.cu.o"
+  "CMakeFiles/gemm_1_shared.dir/gemm_1_shared.cu.o.d"
+  "gemm_1_shared"
+  "gemm_1_shared.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/gemm_1_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/4_gemm/CMakeFiles/gemm_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gemm_2.dir/gemm_2.cu.o"
+  "CMakeFiles/gemm_2.dir/gemm_2.cu.o.d"
+  "gemm_2"
+  "gemm_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/gemm_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
